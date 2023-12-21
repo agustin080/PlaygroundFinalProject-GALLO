24c4156489f4adb87de44c8358bbc194aa6d9ca3
Agustin <agustingallo958@gmail.com>
2023-12-20 23:29:40 -0300

    Código actualizado

--- USUARIOS/admin.py
+++ USUARIOS/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from USUARIOS.models import PerfilUsuario
 
 # Register your models here.
+
+admin.site.register(PerfilUsuario)
--- publicaciones/admin.py
+++ publicaciones/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from publicaciones.models import publicacion
 
 # Register your models here.
+
+admin.site.register(publicacion)
